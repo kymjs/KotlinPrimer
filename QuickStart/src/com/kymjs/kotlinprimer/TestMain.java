e6a0f480b86919979a70d558239119609a1570c1
ZhangTao10 <tao.zhang10@ele.me>
2018-06-05 22:54:39 +0800

    first

---
+++ QuickStart/src/com/kymjs/kotlinprimer/TestMain.java
@@ -0,0 +1,13 @@
+package com.kymjs.kotlinprimer;
+
+/**
+ * Created by ZhangTao on 18/6/5.
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+
+        TestKt.test("hello");
+        new Main().test();
+    }
+}
